9a55913d0c14c17e84ad17fa780c69ecac9d0337
michaldesktop <michal5832@gmail.com>
2024-01-02 16:15:59 +0000

    JUnit 5 Instance Lifecycle Demo - complete

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,16 +1,19 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
     private Greeting greeting;
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("-----Before - this is only called once-----");
+    }
 
     @BeforeEach
     void setUp() {
-        System.out.println("before each method : ");
+        System.out.println("Before each method : ");
         greeting = new Greeting();
     }
 
@@ -22,5 +25,20 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Michal"));
+    }
+
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After each method :");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("-----After - this is only called once-----");
     }
 }
